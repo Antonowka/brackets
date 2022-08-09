673bd44d5e8c9ef28a3065227a2b2370a970692d
Antonowka <navickiy1994@gmail.com>
2022-08-09 19:52:47 +0200

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(s) {
+
+	  const stack = [];
+	  const characters = { ')': '(', '}': '{', ']': '[', '|': '|'};for (const char of s) {
+			
+		 if (!characters[char]){
+			 stack.push(char); 
+		 } 
+		 else if (stack.pop() !== characters[char]){
+			  return false;
+		 } 
+	  }
+	  return stack.length === 0;
 }
