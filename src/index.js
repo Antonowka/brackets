29498cb74c0974bece2b924412c2b85484590efe
Antonowka <navickiy1994@gmail.com>
2022-08-10 16:49:23 +0200

    fix: fix solution

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-module.exports = function check(s) {
 
-	  const stack = [];
-	  const characters = { ')': '(', '}': '{', ']': '[', '|': '|'};for (const char of s) {
-			
-		 if (!characters[char]){
-			 stack.push(char); 
-		 } 
-		 else if (stack.pop() !== characters[char]){
-			  return false;
-		 } 
-	  }
-	  return stack.length === 0;
+module.exports = function isBalanced(s) {
+	const map = {
+		'(': ')',
+		'[': ']',
+		'{': '}',
+	};
+	const closing = Object.values(map);
+	const stack = [];
+
+	for (let char of s) {
+		if (map[char]) {
+			stack.push(char);
+		} else if (closing.includes(char) && char !== map[stack.pop()]) {
+			return false;
+		}
+	}
+	return !stack.length;
 }
